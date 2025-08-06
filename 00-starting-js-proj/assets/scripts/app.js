fe4c1ecfb7f87fa153fc97b28702a80a4f080f15
ShaunF777 <shaunfr777@gmail.com>
2025-08-06 19:29:11 +0200

    feat: refactor app.js to include user and User class implementations

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,3 +1,34 @@
-import {apiKey} from './util';
+import { apiKey } from './util.js';
 
 console.log(apiKey);
+
+// Objects 
+const user = {
+    name: "Max",
+    age: 34,
+    // Methods are functions within an object
+    greet() {
+        console.log("Hello!");
+        console.log(this.age);
+    }
+};
+
+console.log(user.name);
+user.greet();
+
+// Classes create generic blueprints for object creation  
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+// Methods also
+    greet() {
+        console.log('Hi');
+    }
+}
+
+// Then create/populate objects
+const user1 = new User("Manuel", 35);
+console.log(user1);
+user1.greet();
