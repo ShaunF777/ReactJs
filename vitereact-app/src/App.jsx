0e6023e4e2e2b9d55266bd163d611eaaa6307368
ShaunF777 <shaunfr777@gmail.com>
2025-08-06 09:19:02 +0200

    feat: add Scrimba logo to App component for enhanced branding

--- vitereact-app/src/App.jsx
+++ vitereact-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import scrimbaLogo from './assets/scrimba.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
@@ -17,6 +18,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://scrimba.com/home" target="_blank">
+          <img src={scrimbaLogo} className="logo scrimba" alt="Scrimba logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
