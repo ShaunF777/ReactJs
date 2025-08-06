d2b6472da82c64f353a28f25462111dff4f42e04
ShaunF777 <shaunfr777@gmail.com>
2025-08-06 14:04:57 +0200

    Run: npm install -D vite-plugin-qrcode ,  add import { qrcode } from 'vite-plugin-qrcode' to vite.config , and change     "dev": "vite ", to     "dev": "vite --host", in package.json . Test QRcode and see it running from my pc onto my phones browser via localip:port

--- vitereact-app/vite.config.js
+++ vitereact-app/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), qrcode()],
   server:{
-    port:3000
+    port:3000 
   },
   build:{
     outDir:'out'
